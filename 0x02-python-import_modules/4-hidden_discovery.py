a90353bd85578c7189219db2872e77a812eb4aa0
Donbosco A Otunga <donotunga@gmail.com>
2024-04-07 18:36:25 +0300

    Hotfix: 0x02../4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,6 +3,6 @@ import hidden_4
 
 if __name__ == "__main__":
     def_names = dir(hidden_4)
-    for items in sorted(def_names):
+    for item in sorted(def_names):
         if not item.startswith("__"):
-            print(items)
+            print(item)
